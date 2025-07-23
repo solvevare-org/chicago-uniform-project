f83fcb8021732df749546b462eda020712c934bf
0xHuzaifa <huzaifasaleem098@gmail.com>
2025-07-24 00:25:26 +0500

    fix: home trust signal section images

--- src/App.tsx
+++ src/App.tsx
@@ -582,7 +582,7 @@ function TrustSignalsSection() {
       <div className="bg-white border border-gray-200 rounded-xl shadow flex flex-col md:flex-row items-center justify-between py-6 px-4 md:px-12 gap-6">
         <div className="flex-1 flex flex-col items-center text-center">
           <img
-            src="/trust/76ers.png"
+            src="76ers.png"
             alt="76ers Partner"
             className="h-12 mb-2 object-contain"
           />
@@ -592,7 +592,7 @@ function TrustSignalsSection() {
         </div>
         <div className="flex-1 flex flex-col items-center text-center">
           <img
-            src="/trust/inc5000.png"
+            src="inc5000.jpg"
             alt="Inc 5000"
             className="h-12 mb-2 object-contain"
           />
@@ -602,7 +602,7 @@ function TrustSignalsSection() {
         </div>
         <div className="flex-1 flex flex-col items-center text-center">
           <img
-            src="/trust/nyt.png"
+            src="nyt.png"
             alt="NY Times"
             className="h-12 mb-2 object-contain"
           />
@@ -613,13 +613,8 @@ function TrustSignalsSection() {
         <div className="flex-1 flex flex-col items-center text-center">
           <div className="flex items-center gap-2 mb-2">
             <img
-              src="/trust/bbb.png"
+              src="bbb.png"
               alt="BBB Accredited"
-              className="h-8 object-contain"
-            />
-            <img
-              src="/trust/a-plus.png"
-              alt="A+ Rating"
               className="h-8 object-contain"
             />
           </div>
